d38dd4eccb0f7e1b678963f07bf919a8c13d4fa0
groolman <cndextremist@gmail.com>
2025-06-27 15:37:23 -0600

    This commit marks the start of datagen

--- src/client/java/grool/example/GroolDataGenerator.java
+++ src/client/java/grool/example/GroolDataGenerator.java
@@ -7,6 +7,7 @@ public class GroolDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
 
 	}
