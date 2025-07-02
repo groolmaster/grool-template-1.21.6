7671159e5242eaf0f32926349e5bb01b93b515c6
jmichaelwiltse <jmichaelwiltse@outlook.com>
2025-07-01 20:28:36 -0600

    fuck of

--- src/main/java/grool/example/Grool.java
+++ src/main/java/grool/example/Grool.java
@@ -21,7 +21,7 @@ public class Grool implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		LOGGER.info("Grool initialize started. Prepare for groolness!");
+		LOGGER.info("ENGAGING GROOLING. PREPARE FOR GROOLING RESURRECTION!");
 
 
 		//Initialize functions here
